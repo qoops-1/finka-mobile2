2213c9c527388f6d96a575c3229e060094504234
Sadyksaj <Sadyk.sajfullin64@gmail.com>
2017-07-10 01:41:34 +0300

    android fix

--- android/app/src/main/java/com/finkaapp/MainActivity.java
+++ android/app/src/main/java/com/finkaapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.finkaapp;
 
 import com.facebook.react.ReactActivity;
-import com.reactnativenavigation.NavigationReactPackage;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "finkaApp";
-    }
 }
--- android/app/src/main/java/com/finkaapp/MainApplication.java
+++ android/app/src/main/java/com/finkaapp/MainApplication.java
@@ -2,9 +2,8 @@ package com.finkaapp;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -13,32 +12,24 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RealmReactPackage()
-      );
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+     public boolean isDebug() {
+         // Make sure you are using BuildConfig from your own application
+         return BuildConfig.DEBUG;
+     }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+     protected List<ReactPackage> getPackages() {
+         // Add additional packages you require here
+         // No need to add RnnPackage and MainReactPackage
+         return Arrays.<ReactPackage>asList(
+             // eg. new VectorIconsPackage()
+         );
+     }
+
+     @Override
+     public List<ReactPackage> createAdditionalReactPackages() {
+         return getPackages();
+     }
 }
