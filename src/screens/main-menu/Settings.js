0e7fd65d95b7b3b28edd1f7a849e2c7ee7e87683
qoops <ilya.v.gladyshev@gmail.com>
2017-07-10 01:45:43 +0300

    logout button

--- src/screens/main-menu/Settings.js
+++ src/screens/main-menu/Settings.js
@@ -2,14 +2,28 @@ import React from 'react'
 import {
   View,
   Text,
+  Button,
 } from 'react-native'
 
 export default class Settings extends React.Component {
+  constructor(props) {
+    super(props)
+    this.logout = this.logout.bind(logout)
+  }
 
+  logout() {
+
+  }
+  
   render() {
     return (
       <View>
         <Text>Hi there from settings</Text>
+        <Button 
+          title='Выйти'
+          color='red'
+          onPress={this.logout}
+        />
       </View>
     )
   }
